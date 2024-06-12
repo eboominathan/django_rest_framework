89d1ba394cd7450f805ca0110756592c4c5e6dc8
Boominathan <eboominathan@gmail.com>
2024-06-12 13:41:23 +0530

    Crud Completed

--- core/home/views.py
+++ core/home/views.py
@@ -20,16 +20,41 @@ def index(request):
         return Response(courses)
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'PUT','PATCH','DELETE'])
 def person(request):
-    if request.GET:
+     if request.method == 'GET':
         objs = Person.objects.all()
         serializer = PersonSerializer(objs, many=True)
         return Response(serializer.data)
-    else:
+    
+     elif request.method == 'POST':
         data = request.data
         serializer = PersonSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
+    
+     elif request.method == 'PUT':
+        data = request.data
+        serializer = PersonSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+     elif request.method == 'PATCH':
+        data = request.data
+        obj = Person.objects.get(id=data['id'])
+        serializer = PersonSerializer(obj,data=data,partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+     else :
+        data = request.data
+        obj = Person.objects.get(id=data['id'])
+        obj.delete()
+        return Response({'message':'deleted'})
+        
+    
