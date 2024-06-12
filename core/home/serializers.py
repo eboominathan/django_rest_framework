a3b82a2f140aebbaed195a2bebc753ad23216878
Boominathan <eboominathan@gmail.com>
2024-06-12 13:55:01 +0530

    validation sample added

--- core/home/serializers.py
+++ core/home/serializers.py
@@ -7,3 +7,11 @@ class PersonSerializer(serializers.ModelSerializer):
         model = Person
         fields = '__all__'
         
+    # def validate_age(self, data):
+    #     print(data)
+    #     return data
+        
+    def validate(self,data):     
+        if data['age'] < 18:
+                raise serializers.ValidationError("age should be greater than 18")
+        return data
