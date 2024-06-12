6c077f4f1e565583f4e5d217fb9c2fec168f7974
Boominathan <eboominathan@gmail.com>
2024-06-12 14:34:39 +0530

    response customized

--- core/home/serializers.py
+++ core/home/serializers.py
@@ -8,12 +8,18 @@ class ColorSerializer(serializers.ModelSerializer):
 
 class PersonSerializer(serializers.ModelSerializer):
     color = ColorSerializer()
+    country = serializers.SerializerMethodField()
 
     class Meta:
         model = Person
-        fields = '__all__'
-
+        fields = '__all__'        
+     
+    def get_country(self, obj):     
+        color_obj = Color.objects.get(id=obj.color.id)
+        return {'color_name':color_obj.colors_name,'hex_code':'#000'}
+     
     def validate(self, data):
         if data['age'] < 18:
             raise serializers.ValidationError("Age should be greater than 18")
         return data
+    
